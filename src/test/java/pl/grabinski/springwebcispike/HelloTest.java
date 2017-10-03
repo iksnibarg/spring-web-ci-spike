927c7e568cda59a95b3f0d9f4113b6c3f5891b28
maciek <whataterriblefailure@gmail.com>
2017-10-03 23:08:29 +0200

    simple selenium test for Welcome page - headless option for chrome driver

--- src/test/java/pl/grabinski/springwebcispike/HelloTest.java
+++ src/test/java/pl/grabinski/springwebcispike/HelloTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.Assert.*;
 
@@ -11,7 +12,9 @@ public class HelloTest {
 
     @Test
     public void checkWelcomePageContent() {
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
 
         driver.navigate().to("localhost:8080");
         assertEquals("hello Bob!", driver.findElement(By.tagName("body")).getText());

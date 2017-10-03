57924a6e179c13a9a24f33d1e1876ebbf1429200
maciek <whataterriblefailure@gmail.com>
2017-10-03 23:31:16 +0200

    simple selenium test for Welcome page - trying with remote selenium web driver

--- src/test/java/pl/grabinski/springwebcispike/HelloTest.java
+++ src/test/java/pl/grabinski/springwebcispike/HelloTest.java
@@ -3,21 +3,26 @@ package pl.grabinski.springwebcispike;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
 
 import static org.junit.Assert.*;
 
 public class HelloTest {
 
     @Test
-    public void checkWelcomePageContent() {
+    public void checkWelcomePageContent() throws Exception {
         ChromeOptions options = new ChromeOptions();
         options.setBinary("google-chrome-stable");
         options.addArguments("--headless");
         options.addArguments("--disable-gpu");
         options.addArguments("--remote-debugging-port=9222");
-        WebDriver driver = new ChromeDriver(options);
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        WebDriver driver = new RemoteWebDriver(new URL("localhost:8080"), capabilities);
 
         driver.navigate().to("localhost:8080");
         assertEquals("hello Bob!", driver.findElement(By.tagName("body")).getText());

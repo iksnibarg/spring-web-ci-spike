acd0805d8f35022f4f44bea42593bbbba0e85dc2
maciek <whataterriblefailure@gmail.com>
2017-10-03 22:52:10 +0200

    simple selenium test for Welcome page

---
+++ src/test/java/pl/grabinski/springwebcispike/HelloTest.java
@@ -0,0 +1,21 @@
+package pl.grabinski.springwebcispike;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.*;
+
+public class HelloTest {
+
+    @Test
+    public void checkWelcomePageContent() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.navigate().to("localhost:8080");
+        assertEquals("hello Bob!", driver.findElement(By.tagName("body")).getText());
+
+        driver.quit();
+    }
+}
